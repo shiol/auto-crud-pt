ae5e6c19cc592b00037ec15ce8dd3250e966443a
Ribamar Sousa <ola@ribafs.org>
2017-12-22 14:28:39 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,11 +1,28 @@
 <?php
 $host = 'localhost';
-$db   = 'auto-crud';
+$db   = 'teste3';
 $user = 'root';
-$pass = '';
+$pass = 'mysql';
 $sgbd='mysql';      // pgsql, mysql
 $table='clientes';
+$script = 'scripts/my.sql';
 
+// Conectar ao banco sys
+$pdo0 = new PDO("$sgbd:host=$host;dbname=sys;", $user, $pass, $opt);
+
+// Caso não exista o banco $db será criado
+$ret = $pdo0->query('create database if not exists '.$db);
+
+// Se for criado o banco o script $script será importado para o mesmo
+if($ret){
+    $pdo0->query('use '.$db);
+
+    $sqlSource = file_get_contents($script);
+    $pdo0->exec($sqlSource);
+    $pdo0 = null;
+}
+
+// Adotar algumas opções para o PDO
 $opt = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
@@ -13,6 +30,7 @@ $opt = [
     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'
 ];
 
+// Conectar ao banco com PDO
 try {
     $pdo = new PDO("$sgbd:host=$host;dbname=$db;", $user, $pass, $opt);
 
@@ -26,4 +44,5 @@ try {
     echo '<b>Linha</b>: '.$e->getLine().'<br>';
 }
 
+// Incluir o funcoes.php
 require_once('./funcoes.php');
